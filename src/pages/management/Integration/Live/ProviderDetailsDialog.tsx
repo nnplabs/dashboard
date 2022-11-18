9f30d4cea5d7e3081e38c16ecc892e7211686537
gaurav <gaurav.dhalla@leapwallet.io>
2022-11-19 02:50:41 +0530

    provider info dialog

---
+++ src/pages/management/Integration/Live/ProviderDetailsDialog.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { ProviderData } from '../../../../types/api/provider';
+import { IntegrationFormHeader } from '../All/ProviderForm';
+
+function ProviderDetailsDialog({
+    provider,
+    onCloseHandler,
+  }: {
+    provider: ProviderData
+    onCloseHandler: () => void
+    }) {
+  return (
+    <div className={"w-[600px] flex flex-col overflow-y-scroll"}>
+        <IntegrationFormHeader
+          providerName={provider.name}
+          onCloseHandler={onCloseHandler}
+        />
+        <div className="flex flex-col h-full w-full p-7 gap-4">
+            {
+                Object.entries(provider.config).map(([key, value]) => {
+                    return (
+                        <div className="flex flex-col gap-2 my-2">
+                            <div className="text-gray-700 text font-medium">{key}:</div>
+                            <div className="text-gray-500 text overflow-clip">{value}</div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    </div>
+  )
+}
+export default ProviderDetailsDialog
